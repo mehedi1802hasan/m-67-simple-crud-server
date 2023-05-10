482f4bf14993fa3c743747b3bf5b6e5064a3f967
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-05-11 02:24:24 +0600

    added update

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
     const result=await cursor.toArray();
     res.send(result);
   })
+  //update ...
+  app.get('/users/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id: new ObjectId (id)};
+    const user=await userCollection.findOne(query);
+    res.send(user);
+  })
 //post
     app.post('/users',async(req,res)=>{
       const user=req.body;
